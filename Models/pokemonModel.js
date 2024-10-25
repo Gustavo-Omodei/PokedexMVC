cfe82b97c72b1c9a451862a26adef93f60da377d
Felipe Ehara <140767595+igorcampos069@users.noreply.github.com>
2024-10-25 20:24:32 -0300

    imagem do pokemon

--- Models/pokemonModel.js
+++ Models/pokemonModel.js
@@ -1,11 +1,11 @@
 const pokemons = [
-    {id: 1, nome: 'Bulbasaur', tipo: 'Grass/Poison'},
-    {id: 2, nome: 'Squirtle', tipo: 'Water'},
-    {id: 3, nome: 'Charmander', tipo: 'Fire'}
+    { id: 1, nome: 'Bulbasaur', tipo: 'Grass/Poison', image: 'https://img.pokemondb.net/sprites/firered-leafgreen/normal/bulbasaur.png' },
+    { id: 2, nome: 'Squirtle', tipo: 'Water' },
+    { id: 3, nome: 'Charmander', tipo: 'Fire' }
 ];
 
 const getPokemons = () => pokemons;
 const getPokemonsById = (id) => pokemons.find(p => p.id === parseInt(id));
-const createPokemon = (nome,tipo) => pokemons.push(pokemons.length+1, nome, tipo)
+const createPokemon = (nome, tipo) => pokemons.push(pokemons.length + 1, nome, tipo)
 
 module.exports = { getPokemons, getPokemonsById, createPokemon };
