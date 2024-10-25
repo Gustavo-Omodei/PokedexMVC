9e35750425e247a53949f85bbb0be284b41edf26
Felipe Ehara <140767595+igorcampos069@users.noreply.github.com>
2024-10-25 20:09:59 -0300

    cadastro treinadoir

--- Controllers/pokemonController.js
+++ Controllers/pokemonController.js
@@ -1,15 +1,26 @@
 const pokemonModel = require('../Models/pokemonModel');
 
+
+const getCadastrarTreinador = async(req, res) => {
+    try {
+        const pokemons = await pokemonModel.getPokemons();
+        res.render('cadastrarTreinador', { pokemons });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao carregar os pokémons');
+    }
+};
+
 const getAllPokemons = (req, res) => {
     const pokemons = pokemonModel.getPokemons();
-    res.render('index', {pokemons});
+    res.render('index', { pokemons });
 
 };
 
 const getPokemon = (req, res) => {
     const pokemon = pokemonModel.getPokemonsById(req.params.id);
-    if(pokemon){
-        res.render('pokemon', {pokemon});
+    if (pokemon) {
+        res.render('pokemon', { pokemon });
     } else {
         res.status(404).send('Pokémon não encontrado');
     }
@@ -17,5 +28,6 @@ const getPokemon = (req, res) => {
 
 module.exports = {
     getAllPokemons,
-    getPokemon
+    getPokemon,
+    getCadastrarTreinador
 };
--- Routes/pokemonRoutes.js
+++ Routes/pokemonRoutes.js
@@ -5,4 +5,19 @@ const pokemonController = require('../Controllers/pokemonController');
 router.get('/', pokemonController.getAllPokemons);
 router.get('/pokemon/:id', pokemonController.getPokemon);
 
+router.get('/cadastrar-pokemon', (req, res) => {
+    res.render('cadastrarPokemon.ejs');
+});
+router.post('/cadastrar-pokemon', (req, res) => {
+    const { nomeTreinador, genero, nomeRival, pokemonInicial } = req.body;
+    res.redirect('/');
+});
+
+router.get('/cadastrar-treinador', pokemonController.getCadastrarTreinador);
+
+
+router.post('/cadastrar-treinador', (req, res) => {
+    const { Nome, Tipo } = req.body;
+    res.redirect('/');
+});
 module.exports = router;
